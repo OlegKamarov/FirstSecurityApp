5a7973fd556ed8c33ef42fc15349012c7aa44c7c
Oleg Kamarov <oleg471@ya.ru>
2022-09-05 13:19:40 +0300

    Урок 81. Кастомная страница авторизации, отключили защиту от межсайтовой поддержки запросов

--- src/main/java/ru/kamarov/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/kamarov/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -1,22 +1,47 @@
 package ru.kamarov.springcourse.FirstSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.kamarov.springcourse.FirstSecurityApp.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.kamarov.springcourse.FirstSecurityApp.services.PersonDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+  private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider); // TODO
+    protected void configure(HttpSecurity http) throws Exception {
+        // конфигурируем сам Spring Security
+        // конфигурируем авторизацию
+        http.csrf().disable() //отключаем защиту от межсайтовой поддержки запросов
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    //Настраиваем аутентификацию
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
